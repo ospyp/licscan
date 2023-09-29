3f8a118c1987d3741018c5f74e2b8be6584025ef
mrfakename <me@mrfake.name>
2023-09-28 18:16:41 -0700

    Update __init__.py

--- licscan/__init__.py
+++ licscan/__init__.py
@@ -1,7 +1,7 @@
 """
 LicScan by @fakerybakery.
 License: NOSCL-C-2.0.
-Please see the source code repositories for documentation, full license text, and more: https://github.com/fakerybakery/licscan
+Please see the source code repositories for documentation, full license text, and more: https://github.com/ospyp/licscan
 
 VERSION 1.0.0
 """
